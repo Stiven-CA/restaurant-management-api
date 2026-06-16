30578c9be51d3ce4013441a2e7302db63bf2b94b
Samuel <Yepes Zuleta@PCSAYZ>
2026-06-16 16:00:39 -0500

    conexion

---
+++ Backend/Restaurant.Infrastructure/Persistence/Conexion.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+
+namespace Restaurant.Infrastructure.Persistence
+{
+    public class Conexion(string conexion) : DbContext
+    {
+        private readonly string _conexion = conexion;
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql (_conexion);
+            optionsBuilder.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
+        }
+    }
+}
+
